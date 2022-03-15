f1e580f9a2536fe55fec868628da2dba304e088c
David <ivan.molina@itponiente.com>
2022-03-15 13:18:08 +0100

    Java: Persona: Update class with comments

--- Persona.java
+++ Persona.java
@@ -1,2 +1,4 @@
 // Clase Persona Herencia
 // Dos tipos de Herencia : extension (a los metodos del padre le añadimos métodos propios) y especializacion(modificamos los métodos del padre para adaptarlos a nuestras necesidades)
+
+// A demás de la herencia por agregación o composición ( añadir clases como atributos de nuestra clase) explicado en coche
