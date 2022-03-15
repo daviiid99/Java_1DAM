f534adda28679dbbd2af3c7265faa34f7df56a18
David <ivan.molina@itponiente.com>
2022-03-15 13:17:03 +0100

    Java: Persona: Update README

---
+++ Persona.java
@@ -0,0 +1,2 @@
+// Clase Persona Herencia
+// Dos tipos de Herencia : extension (a los metodos del padre le añadimos métodos propios) y especializacion(modificamos los métodos del padre para adaptarlos a nuestras necesidades)
