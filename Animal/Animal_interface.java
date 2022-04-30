a5b95327ccc33660e8d32d11f6ccbe0a47cbf2e0
David <davidmolinamorales1999@gmail.com>
2022-04-30 21:28:56 +0200

    Java: Animal: Correccion errores

--- Animal/Animal_interface.java
+++ Animal/Animal_interface.java
@@ -1,8 +1,8 @@
 public interface Animal_interface{
   // Al declarar variables en una interfaz hay que inicializarlos
   // Pero no sirven porque no se heredan
-  public final static String alimentacion = "comida";
-  public final static int excretando = 0;
+  public final static String ALIMENTACION = "comida";
+  public final static int EXCRETANDO = 0;
   public String durmiendo();
   public String comiendo(String alimentacion);
   public String onomatopeya();
